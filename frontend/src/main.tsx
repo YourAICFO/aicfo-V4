397e0d2f3efa214e6521ffc7356f3c36cf18ce02
YourAICFO <youraipocketcfo@gmail.com>
2026-02-04 23:44:06 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+fetch("https://web-production-be25.up.railway.app/")
+  .then(res => res.json())
+  .then(data => {
+    console.log("BACKEND RESPONSE:", data);
+  })
+  .catch(err => {
+    console.error("BACKEND ERROR:", err);
+  });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
